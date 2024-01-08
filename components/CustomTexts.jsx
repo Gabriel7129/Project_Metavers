02d92789b7853bc1cf2179691b9b257468a73305
Gabriel7129 <moldoveanugabriel7102006@gmail.com>
2024-01-08 17:13:34 +0100

    siu

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -4,11 +4,11 @@ import { motion } from 'framer-motion';
 
 import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TypingText = ( { title, textStyles } ) => (
- <motion.p
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
- >
+  >
     {Array.from(title).map((letter, index) => (
 
       <motion.span
@@ -16,19 +16,18 @@ export const TypingText = ( { title, textStyles } ) => (
       >
         {letter === '' ? '\u00A0' : letter}
       </motion.span>
-))}
- </motion.p>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = ( { title, textStyles } ) => (
+export const TitleText = ({ title, textStyles }) => (
   <motion.h2
-    variants = {textVariant2}
-    initial = "hidden"
+    variants={textVariant2}
+    initial="hidden"
     whileInView="show"
     className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
-  
-  {title}
+    {title}
 
   </motion.h2>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
-const Home = () => (
+const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
@@ -12,17 +12,17 @@ const Home = () => (
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="gradient-04 z-0" />
+      <div className="gradient-03 z-0" />
       <WhatsNew />
     </div>
     <World />
     <div className="relative">
       <Insights />
-      <div className="gradient-04 z-0" />
+      <div className="gradient-03 z-0" />
       <Feedback />
     </div>
     <Footer />
   </div>
 );
 
-export default Home;
+export default Page;
